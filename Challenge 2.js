1319d1c2908e357b22a25e19e2d21197f741a659
Vikkysoft <bolvic28@gmail.com>
2022-09-21 11:11:34 +0100

    add challenge 2

---
+++ Challenge 2.js
@@ -0,0 +1,13 @@
+//Length converter
+function converter(ft) {
+    const results = {
+        meters: ft * 0.3048,
+        cm: ft * 30.48,
+        inch: ft * 12,
+        yard: ft * 0.333,
+    };
+
+    return results;
+}
+
+console.log (converter(2));
